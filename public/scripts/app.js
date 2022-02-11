7e7910ed335cb45c8cf4da4ef42238defa571afb
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-02-11 18:54:14 +0000

    useless at frequent committing - implemented most of the basic react jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,44 +1,107 @@
 "use strict";
 
 var app = {
-  title: "title",
-  subtitle: "subtitle"
+  title: "Title",
+  subtitle: "Subtitle",
+  options: ['One', 'Two']
 };
 
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    { "class": "test more-test" },
-    app.title
-  ),
-  React.createElement(
-    "p",
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    "p",
-    null,
-    app.dummy ? app.dummy : "Doesn't exist"
-  ),
-  React.createElement(
-    "ol",
+var options = [];
+var optionsCount = options.length;
+var selection = void 0;
+
+function render() {
+
+  function submitFunc(e) {
+    e.preventDefault();
+    var text = e.target.elements.option.value;
+
+    if (text) {
+      options.push(text);
+      optionsCount = options.length;
+      console.log(optionsCount);
+      e.target.elements.option.value = '';
+    }
+
+    render();
+  }
+
+  function deleteAll() {
+    options = [];
+    optionsCount = options.length;
+    render();
+  }
+
+  function select() {
+    var num = Math.floor(Math.random() * optionsCount);
+    selection = options[num];
+    render();
+  }
+
+  var template = React.createElement(
+    "div",
     null,
     React.createElement(
-      "li",
+      "h1",
+      { "class": "test more-test" },
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      "p",
       null,
-      "Item 1"
+      app.subtitle
     ),
     React.createElement(
-      "li",
+      "h3",
       null,
-      "Item 2"
+      optionsCount
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length > 0 ? "Here are your options" : "No options"
+    ),
+    React.createElement(
+      "ol",
+      null,
+      options.map(function (item, i) {
+        return React.createElement(
+          "li",
+          { key: i },
+          item
+        );
+      })
+    ),
+    React.createElement(
+      "button",
+      { onClick: select },
+      "What should I do?"
+    ),
+    React.createElement(
+      "h1",
+      null,
+      selection
+    ),
+    React.createElement(
+      "button",
+      { onClick: deleteAll },
+      "Remove all"
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: submitFunc },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        null,
+        "Submit"
+      )
     )
-  )
-);
+  );
 
-var appRoot = document.getElementById('app');
+  var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+  ReactDOM.render(template, appRoot);
+}
+
+render();
--- src/app.js
+++ src/app.js
@@ -1,23 +1,69 @@
 const app = {
-  title: "title",
-  subtitle: "subtitle"
+  title: "Title",
+  subtitle: "Subtitle",
+  options: ['One', 'Two']
 }
 
-const template = (
+  let options = []
+  let optionsCount = options.length
+  let selection
 
-  <div>
-    <h1 class='test more-test'>{app.title}</h1>
-    <p>{app.subtitle}</p>
-    <p>{app.dummy ? app.dummy : "Doesn't exist"}</p>
-    <ol>
-      <li>Item 1</li>
-      <li>Item 2</li>
-    </ol>
-  </div>
+function render() {
 
-);
+  function submitFunc(e) {
+    e.preventDefault()
+    let text = e.target.elements.option.value
+
+    if (text) {
+      options.push(text)
+      optionsCount = options.length
+      console.log(optionsCount)
+      e.target.elements.option.value = ''
+    }
+
+    render()
+  }
+
+  function deleteAll() {
+    options = [];
+    optionsCount = options.length;
+    render()
+  }
 
 
-const appRoot = document.getElementById('app');
+  function select() {
+    const num = Math.floor(Math.random()*optionsCount)
+    selection = options[num]
+    render()
+  }
 
-ReactDOM.render(template, appRoot);
+  const template = (
+
+    <div>
+      <h1 class='test more-test'>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <h3>{optionsCount}</h3>
+      <p>{(app.options.length > 0) ? "Here are your options" : "No options"}</p>
+      <ol>
+      {options.map((item,i) => {
+        return <li key={i}>{item}</li>
+      })}
+      </ol>
+      <button onClick={select}>What should I do?</button>
+      <h1>{selection}</h1>
+      <button onClick={deleteAll}>Remove all</button>
+      <form onSubmit={submitFunc}>
+        <input type="text" name="option"></input>
+        <button>Submit</button>
+      </form>
+    </div>
+
+  );
+
+  const appRoot = document.getElementById('app');
+
+  ReactDOM.render(template, appRoot)
+
+}
+
+  render();
