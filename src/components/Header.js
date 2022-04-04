577ae03d9508e2df8fb2132582f7b5a0573bafa3
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-04-04 17:17:03 +0100

    removed pointless default props from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
-Header.defaultProps = {
-  title: 'Random',
-  subtitle: 'Randomly choose stuff, I guess?'
-};
-
-function Header(props) {
+function Header() {
   return (
     <div className="header">
-      <h1 className="header__title">{props.title}</h1>
-      <h2 className="header__subtitle">{props.subtitle}</h2>
+      <h1 className="header__title">Random</h1>
+      <h2 className="header__subtitle">Randomly choose stuff, I guess?</h2>
     </div>
   );
 }
