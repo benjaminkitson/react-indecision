b92be8dc2c8bc5872813491384deb6575dea7859
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-08-19 00:00:41 +0100

    initialised tailwind, bypassed existing scss

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Content from './components/Content';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import './styles/index.css';
 
 const app = document.getElementById('app');
 
