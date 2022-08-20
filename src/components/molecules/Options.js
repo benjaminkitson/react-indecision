b79c98d61301de4cf2052045e738c51a0cc5f0e8
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-08-20 20:35:37 +0100

    roughly styled options component

--- src/components/molecules/Options.js
+++ src/components/molecules/Options.js
@@ -4,17 +4,20 @@ import RandomContext from "../../contexts/random";
 
 export default function Options({ options, resetOptions }) {
     return (
-        <>
-            <div className="flex flex-col items-center justify-center p-10">
+        <div className="flex h-[700px] w-full flex-col items-center justify-center bg-blue-400">
+            <div className="mb-5 flex h-[500px] w-5/6 flex-col items-center justify-center bg-blue-500 p-10">
                 <ol>
                     {options.map((option) => (
                         <Option key={option} option={option} />
                     ))}
                 </ol>
             </div>
-            <button className="reset-button button" onClick={resetOptions}>
+            <button
+                className="w-32 rounded-md bg-blue-800 p-4 text-xl text-white"
+                onClick={resetOptions}
+            >
                 Reset
             </button>
-        </>
+        </div>
     );
 }
