b059e2bf753776b8b12a8bc74b8951147f59595b
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-08-22 23:09:37 +0100

    corrected flex display issue of options

--- src/components/atoms/Option.js
+++ src/components/atoms/Option.js
@@ -5,7 +5,7 @@ function Option({ option }) {
     const { deleteOption } = useContext(RandomContext);
 
     return (
-        <li className="flex h-10 w-2/3 justify-center">
+        <li className="mb-3 flex h-10 w-2/3 justify-center">
             <div className="group mr-4 flex h-full w-2/3 items-center justify-between rounded-md bg-white px-4">
                 {option}
                 <button
