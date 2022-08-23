c02e39e176d571c7fe88e7d3fdc526a45427b517
Benjamin Kitson <benk@andertons.co.uk>
2022-08-23 17:41:53 +0100

    created dedicated button component

---
+++ src/components/atoms/Button.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Button({ children, className, colour, groupHidden }) {
+    const defaultClasses = `${
+        groupHidden ? "hidden group-hover:flex" : "flex"
+    } items-center justify-center rounded-md`;
+
+    let colourClass;
+
+    switch (colour) {
+        case "blue":
+            colourClass = "bg-blue-600 hover:bg-blue-700 active:bg-blue-800";
+            break;
+        case "grey":
+            colourClass = "bg-gray-400 hover:bg-gray-500 active:bg-gray-600";
+            break;
+        default:
+            throw new Error("Button component must have a colour!");
+    }
+
+    return (
+        <button className={`${defaultClasses} ${colourClass} ${className}`}>
+            {children}
+        </button>
+    );
+}
