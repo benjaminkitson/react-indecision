105f002b8f82d8b43395d4219bc9d57a8c14f6eb
Benjamin Kitson <benk@andertons.co.uk>
2022-08-30 09:05:52 +0100

    copied logic from existing input component into custom one

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Input() {
-    return <input></input>;
+    return (
+        <input
+            className={`h-full w-full rounded-md px-4 ${
+                isInvalid
+                    ? "outline-red-600 focus:outline-red-600"
+                    : "focus:outline-blue-700"
+            }`}
+            type="text"
+            name="option"
+            value={toUpdate}
+            onChange={(e) => setToUpdate(e.target.value)}
+        ></input>
+    );
 }
