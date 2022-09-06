5af32a8c78727b7fef65093d3ce6aaa47ae1bcc9
Benjamin Kitson <benk@andertons.co.uk>
2022-09-06 22:11:14 +0100

    styled dice container, added start message

--- src/pages/Dice.js
+++ src/pages/Dice.js
@@ -20,18 +20,28 @@ export default function Dice() {
         BsFillDice6Fill,
     ];
 
+    function BeforeRoll() {
+        return (
+            <div>
+                <h1>Roll the dice!</h1>
+            </div>
+        );
+    }
+
     const [selection, setSelection] = useState(undefined);
 
     const roll = () => {
-        const num = Math.floor(Math.random() * 6);
+        const num = Math.floor(Math.random() * 6) + 1;
         setSelection(num);
     };
 
-    const Dice = selection ? options[selection] : null;
+    const Dice = selection ? options[selection - 1] : BeforeRoll;
 
     return (
         <div className="flex h-full w-full flex-col items-center justify-center text-9xl text-blue-700">
-            {!!selection.toString() && <Dice />}
+            <div className={"flex grow items-center justify-center bg-white"}>
+                <Dice />
+            </div>
             <div className="flex h-40 w-full flex-col items-center justify-center bg-blue-500 p-5">
                 <RollDice roll={roll} />
             </div>
