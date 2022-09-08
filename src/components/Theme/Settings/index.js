f8f1bc5fb82f58fe8188d368ced3d99530ebad8c
Benjamin Kitson <benk@andertons.co.uk>
2022-09-08 04:40:57 +0100

    styled modal a bit

--- src/components/Theme/Settings/index.js
+++ src/components/Theme/Settings/index.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Settings() {
     return (
-        <div className="absolute top-0 left-0 z-50 flex h-full w-full items-center justify-center">
-            <div className="flex h-96 w-96 items-center justify-center">
+        <div className="absolute top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-75">
+            <div className="flex h-96 w-96 items-center justify-center rounded-lg bg-blue-700">
                 Settings
             </div>
         </div>
