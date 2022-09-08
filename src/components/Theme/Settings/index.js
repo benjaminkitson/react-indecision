286f031b613d626ce24d8583b14b2b878ce37593
Benjamin Kitson <benk@andertons.co.uk>
2022-09-08 04:38:41 +0100

    created basic settings modal

---
+++ src/components/Theme/Settings/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Settings() {
+    return (
+        <div className="absolute top-0 left-0 z-50 flex h-full w-full items-center justify-center">
+            <div className="flex h-96 w-96 items-center justify-center">
+                Settings
+            </div>
+        </div>
+    );
+}
--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
+import Settings from "./Settings";
 
 export default function Theme({ children }) {
     return (
-        <div className="flex h-screen max-h-screen w-full flex-col justify-between">
+        <div className="relative flex h-screen max-h-screen w-full flex-col justify-between">
+            <Settings />
             <Header />
             <div className="flex w-full flex-grow flex-col items-center justify-between bg-black p-10">
                 {children}
