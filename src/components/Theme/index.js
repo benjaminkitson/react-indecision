0004f4a1e5bd4d4d6ebecb51df949e7eadb07889
Benjamin Kitson <benk@andertons.co.uk>
2022-09-08 04:43:11 +0100

    created modal open state

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 import Settings from "./Settings";
+import { useState } from "React";
 
 export default function Theme({ children }) {
+    const [isSettings, setIsSettings] = useState(false);
     return (
         <div className="relative flex h-screen max-h-screen w-full flex-col justify-between">
-            <Settings />
-            <Header />
+            {isSettings && <Settings />}
+            <Header setIsSettings={setIsSettings} />
             <div className="flex w-full flex-grow flex-col items-center justify-between bg-black p-10">
                 {children}
             </div>
