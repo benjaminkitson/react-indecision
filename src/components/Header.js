9411e67c7c220aea1ca6148f63edc733c22725f8
Benjamin Kitson <benk@andertons.co.uk>
2022-09-08 14:52:16 +0700

    refactored header to use cog icon, repositioned content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { HiCog } from "react-icons/hi";
 
 export default function Header({ openSettings }) {
     return (
-        <div className="flex w-full flex-col items-center justify-center bg-blue-500 p-10 text-white">
-            <h1 className="mb-10 text-6xl">Random</h1>
-            <h2 className="text-3xl">Randomly choose stuff, I guess?</h2>
-            <h2 className="cursor-pointer" onClick={() => openSettings()}>
-                Settings
-            </h2>
+        <div
+            className={
+                "flex w-full items-center justify-between bg-blue-500 px-20 py-5 text-white"
+            }
+        >
+            <div className="w-1/6"></div>
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="mb-5 text-6xl">Random</h1>
+                <h2 className="text-3xl">Randomly choose stuff, I guess?</h2>
+            </div>
+            <div className="flex w-1/6 justify-end">
+                <HiCog
+                    onClick={() => openSettings()}
+                    className={"cursor-pointer text-5xl"}
+                />
+            </div>
         </div>
     );
 }
